a6a899fa205ef81be7ba297fa47865a9dc597133
Mokshitha99 <122758221+Mokshitha99@users.noreply.github.com>
2023-04-23 18:38:53 -0400

    Add files via upload

---
+++ R/ds.R
@@ -0,0 +1,83 @@
+#' Compute the p-value of a t-test
+#'
+#' This function computes the p-value of a t-test between two vectors.
+#'
+#' @param x a numeric vector representing the first group of data
+#' @param y a numeric vector representing the second group of data
+#' @return a numeric value representing the p-value of the t-test
+#' @examples
+#' t_test_p_value(c(2.5, 3, 3.5, 2), c(1, 1.5, 2, 2.5))
+#' @export
+t_test_p_value <- function(x, y) {
+  t_test <- t.test(x, y)
+  p_value <- t_test$p.value
+  return(p_value)
+}
+#' Compute the correlation coefficient
+#'
+#' This function computes the correlation coefficient between two vectors.
+#'
+#' @param x a numeric vector
+#' @param y a numeric vector
+#'
+#' @return a numeric value representing the correlation coefficient
+#'
+#' @examples
+#' correlation_coefficient(c(2.5, 3, 3.5, 2), c(1, 1.5, 2, 2.5))
+#'
+#' @export
+correlation_coefficient <- function(x, y) {
+  cor(x, y)
+}
+#' Perform multiple linear regression analysis
+#'
+#' This function performs a multiple linear regression analysis on the specified data and formula.
+#'
+#' @param formula a formula specifying the regression model
+#' @param data a data frame containing the variables specified in the formula
+#'
+#' @return a list containing the results of the regression analysis
+#'
+#' @examples
+#' data <- data.frame(x1 = c(1, 2, 3, 4), x2 = c(2, 3, 4, 5), x3 = c(3, 4, 5, 6), y = c(5, 6, 7, 8))
+#' multiple_regression_analysis(formula = y ~ x1 + x2 + x3, data = data)
+#'
+#' @export
+multiple_regression_analysis <- function(formula, data) {
+  lm(formula, data)
+}
+#' Create a scatter plot of two variables
+#'
+#' This function creates a scatter plot of two variables
+#'
+#' @param x a numeric vector representing the first variable
+#' @param y a numeric vector representing the second variable
+#' @return a ggplot object representing the scatter plot
+#' @examples
+#' scatter_plot(c(2.5, 3, 3.5, 2), c(1, 1.5, 2, 2.5))
+#' @export
+scatter_plot <- function(x, y) {
+#' @import ggplot2
+  data <- data.frame(x = x, y = y)
+  result <- ggplot(data, aes(x = x, y = y)) + geom_point()
+  return(result)
+}
+
+#' Create a new variable as a sum of two other variables
+#'
+#' This function creates a new variable that is the sum of two existing variables.
+#'
+#' @param x a numeric vector
+#' @param y a numeric vector
+#'
+#' @return a numeric vector representing the sum of x and y
+#'
+#' @examples
+#' create_sum_variable(c(2.5, 3, 3.5, 2), c(1, 1.5, 2, 2.5))
+#'
+#' @export
+create_sum_variable <- function(x, y) {
+  x + y
+}
+
+
